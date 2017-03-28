d2c516c3caf709fe72f2df21b76152bd69d05e9c
luca arcara <l.arcara@hitachi-systems-cbt.com>
2017-03-28 16:16:32 +0200

    add Rmagick

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative "./circle.rb"
 require 'sinatra'
 require 'haml'
 require "rubygems"
-require 'RMagick'
+require 'rmagick'
 
 post  '/' do
   x_size = params[:custom_size][0].to_f.send(:cm)
